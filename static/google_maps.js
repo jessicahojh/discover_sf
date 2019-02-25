bb394a23f2f5b812d06c5485d31d3f2c24add037
Jessica Ho <jessica.ho.jh@gmail.com>
2019-02-25 20:16:32 +0000

    got images to display on home page and neighborhood page

--- static/google_maps.js
+++ static/google_maps.js
@@ -5,40 +5,38 @@ function initMap() {
     // Specify where the map is centered
     // Defining this variable outside of the map optios markers
     // it easier to dynamically change if you need to recenter
-    let myLatLng = {lat: 37.601773, lng: -122.202870};
+    let myLatLng = {lat: 37.7525, lng: -122.4476};
 
     // Create a map object and specify the DOM element for display.
     let map = new google.maps.Map(document.getElementById('places-map'), {
         center: myLatLng,
         scrollwheel: false,
-        zoom: 5,
+        zoom: 12,
         zoomControl: true,
         panControl: false,
         streetViewControl: false,
-        styles: MAPSTYLES,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
     });
 
 
 
-//     // Retrieving the information with AJAX
-//     $.get('/places_location.json', function (places) {
-//       // Attach markers to each place location in returned JSON
+    // // Retrieving the information with AJAX
+    // $.get('/places_location.json', function (places) {
+    //   // Attach markers to each place location in returned JSON
 
-//       let place, marker, html;
+    //   let place, marker, html;
 
-//       for (let key in places) {
-//         place = places[key];
+    //   for (let key in places) {
+    //     place = places[key];
 
-//         // Define the marker
-//             marker = new google.maps.Marker({
-//                 position: new google.maps.LatLng(place.p_lat, place.p_long),
-//                 map: map,
-//                 title: 'Place: ' + place.placename,
-//             });
+    //     // Define the marker
+    //         marker = new google.maps.Marker({
+    //             position: new google.maps.LatLng(place.p_lat, place.p_long),
+    //             map: map,
+    //             title: 'Place: ' + place.placename,
+    //         });
 
-//         bindInfoWindow(marker, map, infoWindow, html);
+    //     bindInfoWindow(marker, map, infoWindow, html);
 
-//       }
-//     });
+    //   }
+    // });
 }
