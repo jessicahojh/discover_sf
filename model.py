0a8f48dd8463515124db6caaf0d5b32aec0f9b87
Jessica <jessica.ho.jh@gmail.com>
2019-08-20 10:56:37 -0700

    trying to connect db

--- model.py
+++ model.py
@@ -135,10 +135,10 @@ def connect_to_db(app):
     db = SQLAlchemy(app)
 
 
-if __name__ == "__main__":
-    # As a convenience, if we run this module interactively, it will leave
-    # you in a state of being able to work with the database directly.
+# if __name__ == "__main__":
+#     # As a convenience, if we run this module interactively, it will leave
+#     # you in a state of being able to work with the database directly.
 
-    from server import app
-    connect_to_db(app)
-    print("Connected to DB.")
+#     from server import app
+#     connect_to_db(app)
+#     print("Connected to DB.")
--- server.py
+++ server.py
@@ -363,7 +363,6 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     #DebugToolbarExtension(app)
 
-    # app.run()
     app.run(host="0.0.0.0")
 
 
