0f1a82fccba39ab87c93e5d8a7a1c4f35db57504
Jessica <jessica.ho.jh@gmail.com>
2019-08-20 14:09:48 -0700

    add db to server.

--- server.py
+++ server.py
@@ -361,7 +361,7 @@ def yelp_api(neighborhood_name):
     
 #
 def connect_to_db(app):
-"""Connect the database to our Flask app."""
+    """Connect the database to our Flask app."""
 
 # Configure to use our PostgreSQL database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///sanfrancisco'
